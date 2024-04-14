b3a5f462a4672bfaed5626b8082ca9831ca1d07e
Kirill <morozoff.gamemode@yandex.ru>
2024-04-14 15:50:59 +0300

    swagger-autogen не видит коментарии

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,30 @@ var router = express.Router();
 
 const {getJsonResponse, accessError} = require("../models/fs");
 
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Получение главной страницы
+ *     description: Возвращает HTML страницу с заголовком "Главная страница"
+ *     responses:
+ *       '200':
+ *         description: Успешный запрос, возвращает HTML страницу
+ *         content:
+ *           text/html:
+ *             schema:
+ *               type: string
+ */
 router.get("/", function(request, response){
      
     response.send("<h1>Главная страница</h1>");
 });
+
 router.get("/about", function(request, response){
      
     response.send("<h1>О сайте</h1>");
 });
+
 router.get("/contact", function(request, response){
      
     response.send("<h1>Контакты</h1>");
