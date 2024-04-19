f30f94bb97b02826cd066a441773547b07201f66
Kirill <morozoff.gamemode@yandex.ru>
2024-04-19 15:05:44 +0300

    env settings

--- models/settings.js
+++ models/settings.js
@@ -2,13 +2,14 @@ const fs = require("fs");
 const settingsData = fs.readFileSync("C:/Users/Кирилл/Desktop/Стажировка/FSService/settings.json");
 const settings = JSON.parse(settingsData);
 
-/*require('dotenv').config()
+require('dotenv').config()
 
 for (const key in process.env) {
     if (key.startsWith("FSS_")) {
-        const envKey = key.substring(4); // Убираем префикс "FSS_"
+        const envKey = key.substring(4).toLowerCase().replace(/_(.)/g, (match, char) => char.toUpperCase()); // Убираем префикс "FSS_" и преобразуем остальную переменную под мид в settings
+        
         settings[envKey] = process.env[key];
     }
-}*/
+}
 
 module.exports = settings;
