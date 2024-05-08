101474db0eaa8cbe1daa9b7e414b5ad418a7753a
Kirill <morozoff.gamemode@yandex.ru>
2024-05-08 16:54:34 +0300

    ошибка разрыва соединения решена

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const settings = require("./models/settings.js");
 var indexRouter = require('./routes/index');
+var indexDb = require('./routes/db');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger/index.js');
 const cookieParser = require('cookie-parser');
@@ -10,6 +11,7 @@ app.use(cookieParser());
 
 app.use(express.json())
 app.use('/', indexRouter);
+app.use('/', indexDb);
 
 //Вывод всех отсортированных куки
 app.get('/cookies', (req, res) => {
--- models/debug.js
+++ models/debug.js
@@ -1,14 +1,14 @@
 //Файл для отладки
 const {getJsonResponse, accessError} = require("../models/fs");
 const checkPath = require("../models/checkPath")
-const paths = "models/test/derectoris/products/category/category1"
+const paths = "models/test/derectoris"
 const settings = require("./settings");
 const fs = require('fs');
 const path = require('path');
 
-/*async function main() {
+async function main() {
     try {
-      const c = await getJsonResponse(path)
+      const c = await getJsonResponse(paths)
       console.log(c);
       if(c.error != undefined){ console.log("123")}
       return c;
@@ -22,22 +22,11 @@ const path = require('path');
   }
   
 console.log(main())
-//console.log(checkPath(path))*/
-
-const {getPathInfo} = require("../models/fs");
-const root = settings.root;
+//console.log(checkPath(path))
 
 const {Client} = require('pg');
 
 const client = new Client({
-    user: settings.user,
-    host: settings.host,
-    database: '',
-    password: settings.password,
-    port: settings.pgPort
-});
-
-const client2 = new Client({
   user: settings.user,
   host: settings.host,
   database: settings.database,
@@ -45,113 +34,17 @@ const client2 = new Client({
   port: settings.pgPort
 });
 
-async function getAllDatabases(client) {
+async function test(client) {
   try {
-      const res = await client.query('SELECT datname FROM pg_database');
+      client.connect();
 
-      const databaseArray = res.rows.map(row => row.datname);
-      return databaseArray;
-  } catch (err) {
-      console.error("Error retrieving list of databases: "+err);
-  }
-};
-
-function checkElementExists(array, element) {
-  return array.some(item => item.toLowerCase() === element.toLowerCase());
-}
-
-async function createDatabase(client, dbName) {
-  try {
-      await client.query("CREATE DATABASE "+dbName);
-
-      console.log("База данных "+dbName+" успешно создана.");
-  } catch (err) {
-      console.error('Ошибка при создании базы данных:', err);
-  }
-}
-
-async function getAllTablesFromDB(client) {
-
-  try {
-      const res = await client.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE'");
-      const tables = res.rows.map(row => row.table_name);
-      return tables;
-  } catch (error) {
-      console.error(error);
-  }
-}
-
-async function createTable(client, table_name) {
-  try {
-    await client.query(`CREATE TABLE IF NOT EXISTS `+table_name+` (
-                          id SERIAL PRIMARY KEY,
-                          name VARCHAR(255) NOT NULL,
-                          id_parent INTEGER,
-                          path TEXT,
-                          folder BOOLEAN,
-                          size FLOAT,
-                          created_at TIMESTAMP,
-                          updated_at TIMESTAMP
-                      );`);
-
-    console.log("Таблица "+table_name+" успешно создана.");
-  } catch (err) {
-    console.error('Ошибка при создании таблицы:', err);
-  }
-}
-
-async function readDirRecursive(dir, level, client, id_parent = null) {
-  try {
-    const files = fs.readdirSync(dir);
-  
-    files.forEach(async file => {
-        const fullPath = path.join(dir, file);
-        const stats = fs.statSync(fullPath);
-
-        const info = await getPathInfo(fullPath, false, false, false);
-        //console.log(info);
-        const id = await client.query(`SELECT id FROM `+settings.tableName+` WHERE path = `+info.path+`;`);
-
-        if (id === undefined) {
-          await client.query(`INSERT INTO fileinfo (name, id_parent, path, folder, size, created_at, updated_at)
-                              VALUES (`+info.name+`, `+id_parent+`, `+info.path+`, `+info.isFolder+`, `+info.size+`, 
-                              `+info.created+`, `+info.updated+`);`);
-        }
-
-        if(stats.isDirectory()) {
-            readDirRecursive(fullPath, level + 1, client, id);
-        }
-    });
-  } catch {
-    console.error('Ошибка при проверке обновлений:', err);
-  }
-}
-
-async function main(client) {
-  try {
-    client.connect();
-    client2.connect();
-
-    const databases = await getAllDatabases(client);
-    console.log(databases);
-    if (!checkElementExists(databases, settings.database)) {
-      createDatabase(client, settings.database);
-    }
-    client.end();
-
-    const tables = await getAllTablesFromDB(client2);
-    console.log(tables);
-    if (!checkElementExists(tables, settings.tableName)) {
-      createTable(client2, settings.tableName);
-    }
-
-    readDirRecursive(root, 0, client2);
-  } catch (error) {
-    console.error(error);
+      const x = await client.query(`SELECT id, name, id_parent, path, folder, size, created_at, updated_at FROM public.fileinfo;`)
+      console.log(x);
+  } catch(err) {
+      console.error(err);
   } finally {
-    client.end();
-    client2.end();
+      client.end();
   }
 }
 
-main(client)
+//test(client)
